69606b1aa51779895e9fe0816768552e033df7ed
agutierrez <agutierrez@leadtech.com>
2024-12-17 14:30:26 +0100

    VISAS-61001

--- pages/page-about.js
+++ pages/page-about.js
@@ -124,7 +124,7 @@ function About1() {
                             <div className="col-lg-4 col-md-12 col-sm-12">
                                 <div className="list-icons mt-50">
                                     <div className="item-icon"><span className="icon-left"><img src="/assets/imgs/page/homepage2/icon-retent.svg" alt="Agon" /></span>
-                                        <h4 className="text-heading-4">3. 24/7 support:</h4>
+                                        <h4 className="text-heading-4">3. 24/7 Support</h4>
                                         <p className="text-body-text color-gray-600 mt-15">Our team is available to answer your questions and provide assistance.</p>
                                     </div>
                                 </div>
