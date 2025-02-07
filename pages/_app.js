6348a785efb78b0c38884584fd196e7d37571aca
mrosendo <mrosendo@leadtech.com>
2025-02-07 10:26:23 +0100

    Making home form cards responsive and adding GTM

--- pages/_app.js
+++ pages/_app.js
@@ -2,22 +2,58 @@ import "../public/assets/css/style.css";
 import "../public/assets/css/modal.css";
 import "../public/assets/css/swiper-custom.css";
 import React, { useEffect, useState } from "react";
+import Script from "next/script";
 import Preloader from "../components/elements/Preloader";
+
+// const GTM_ID = "GTM-XXXXXXX";
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1000);
 
     if (typeof window !== "undefined") {
       window.WOW = require("wowjs");
     }
     new WOW.WOW().init();
   }, []);
-  return <>{!loading ? <Component {...pageProps} /> : <Preloader />}</>;
+  if (loading) return <Preloader />;
+  else {
+    return (
+      <>
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+          `,
+          }}
+        />
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <Component {...pageProps} />{" "}
+      </>
+    );
+  }
 }
 
 export default MyApp;
